710fba54753c6767fcd77ce320730eec8a0cc4f2
Nidhi <nidhikanwar45@gmail.com>
2022-09-12 16:36:44 +0530

    All files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Todo-List';
+  // constructor() {
+  //   setTimeout(() =>{
+  //     this.title= "Changed title";
+  //   },2000);
+  // }
 }
